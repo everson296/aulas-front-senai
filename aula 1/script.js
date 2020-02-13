ea4cfa11bb525a15782c76daa681be745fc5ed8e
EVERSON SILVA DE ALMEIDA <19266682@CFP127ALUNO.LOCAL>
2020-02-13 09:30:56 -0300

    Adicionar um botao de exibir e ocultar

--- aula 1/script.js
+++ aula 1/script.js
@@ -52,139 +52,198 @@
 // console.log(nota1++); // ele adiciona/incrementa depois de usar, ou seja no caso nota1 será = 0; depois será 1;
 // console.log(++nota2); // ele adiciona/incrementa antes de usar, ou seja no caso nota2 será = 1; depois será 2;
 
-const $nome = document.getElementById('nome').value;
+// const $nome = document.getElementById('nome').value;
+
+
+// const $exibir = document.getElementById('exibir');
+// const $calcular = document.getElementById('calcular');
+// const $container = document.querySelector('.container');
+// const $ocultar = document.
+
+// const calcularMedia = (n1,n2) => (parseInt(n1) + parseInt(n2)) /2;
+// const verificarSituacao = (media) => media >= 5 ? "Aprovado" : "Reprovado" ;
+
+
+// // console.log($nome);
+
+// const exibirMedia = () => {
+//     const $nome = document.getElementById('nome');
+//     const nota1 = document.getElementById('nota1').value;
+//     const nota2 = document.getElementById('nota2').value;
+//     const $media = document.getElementById('media');
+//     const $situacao = document.getElementById('situacao');
+    
+
+//     const media= calcularMedia( nota1, nota2);
+//     $situacao.value = verificarSituacao(media);
+    
+//     if(media >= 5){
+//         $situacao.value = "aprovado";
+//         // $situacao.classList.remove('reprovado');
+//         // $situacao.classList.add('aprovado');
+//          $situacao.style.color = 'green'; //posso fazer um "style" assim e tbm de outra maneira;
+//     }else{
+//         $situacao.value = "reprovado";
+//         // $situacao.classList.remove('aprovado');
+//         // $situacao.classList.add('reprovado');
+//         $situacao.style.color = 'red'; //posso fazer um "style" assim e tbm de outra maneira;
+//     }
+
+//     // meu jeito, funciona !!
+
+//     // if($nota1 > 10 || $nota1 < 0 ){
+//     //     media.value = "erro, nota invalida";
+//     // }
+
+//     // if($nota2 > 10 || $nota2 < 0 ){
+//     //     media.value = "erro, nota invalida";
+//     // }
+    
+//     // if(media > 10 || media < 0){
+//     //         $conceito.value = "erro, notas invalidas";
+//     //     }else{
+//     // }if(media == 0 || media < 3){
+//     //     $conceito.value = "E"
+//     // }else {
+//     //     if(media == 3 || media < 5){
+//     //         $conceito.value = "D"
+//     //     }else{
+//     //         if(media == 5 || media < 8){
+//     //             $conceito.value = "C"
+//     //         }else{
+//     //             if(media == 8 || media < 10){
+//     //                 $conceito.value = "B"
+//     //             }else{
+//     //                 if(media == 10){
+//     //                     $conceito.value = "A"
+//     //                 }
+//     //             }
+//     //         }
+//     //     }
+//     // }
+
+    
+
+//     // console.log($nome.value);
+
+//     $media.value = media;
+// }
+
+//     const calcularConceito = () =>{
+//     const media = document.getElementById ('media').value;
+//     const $conceito = document.getElementById('conceito');
+    
+//     if(media == 0 || media < 3){
+//             $conceito.value = "E";
+//          }else {
+//             if(media == 3 || media < 5){
+//                  $conceito.value = "D";
+//              }else{
+//                  if(media == 5 || media < 8){
+//                      $conceito.value = "C";
+//                  }else{
+//                      if(media == 8 || media < 10){
+//                          $conceito.value = "B";
+//                      }else{
+//                          if(media == 10){
+//                              $conceito.value = "A";
+//                          }
+//                      }
+//                  }
+//              }
+//          }
+    
+// }
+
+// function exibirResultados(){
+//     exibirMedia();
+//     calcularConceito();
+// }
+
+
+// // const exibirResultados = () =>{
+// //     exibirMedia();
+// //     calcularConceito();
+// // }
+
+// // const calcular2 = function () { // segunda maneira  de encurtar a função
+// //     calcularConceito();
+// //     calcularMedia;
+// // }
+
+// // const calcular3 = () => { // iremos usar esta maneira de criar uma função
+// //     calcularMedia();
+// //     calcularConceito();
+// // }
+
+// // function soma (a, b) {
+// //     return a + b;
+// // }
+
+// //const soma2 = (a, b) => a + b;// uma das aneiras de encurtar a função. 
+
+// //const soma2 = a => a + b; // funciona quando so tem um argumento, ele não precisa de parenteses.
+
+// const exibirCalculadora = (el) => el.style.color = 'red';
+
+
+
+// $calcular.addEventListener('click', exibirResultados);
+// $exibir.addEventListener('click',() => exibirCalculadora ($conteiner));
+
+
 const $calcular = document.getElementById('calcular');
+const $exibir = document.getElementById('exibir');
+const $ocultar = document.getElementById('ocultar');
+const $conteiner = document.querySelector ('.conteiner');
 
-const calcularMedia = (n1,n2) => (parseInt(n1) + parseInt(n2)) /2;
-
-
-
-
-const verificarSituacao = media >= 5 ? "Aprovado" : "Reprovado" ;
+const calcularMedia = ( n1, n2 ) => ( parseInt( +n1 ) + parseInt( +n2 ) ) / 2;
  
+const verificarSituacao = ( media ) => media >= 5 ? "Aprovado" : "Reprovado";
 
-console.log($nome);
-
-const exibirMedia = () =>{
-    const $nome = document.getElementById('nome');
-    const nota1 = document.getElementById('nota1');
-    const nota2 = document.getElementById('nota2');
+const exibirMedia = () => {
+    const nota1 = document.getElementById('nota1').value;
+    const nota2 = document.getElementById('nota2').value;
     const $media = document.getElementById('media');
     const $situacao = document.getElementById('situacao');
-    
 
-    const media= calcularMedia( nota1, nota2);
+    const media = calcularMedia ( nota1, nota2 ) ;
 
-    
-    if(media >= 5){
-        $situacao.value = "aprovado";
-        // $situacao.classList.remove('reprovado');
-        // $situacao.classList.add('aprovado');
-         $situacao.style.color = 'green'; //posso fazer um "style" assim e tbm de outra maneira;
-    }else{
-        $situacao.value = "reprovado";
-        // $situacao.classList.remove('aprovado');
-        // $situacao.classList.add('reprovado');
-        $situacao.style.color = 'red'; //posso fazer um "style" assim e tbm de outra maneira;
-    }
-
-    // meu jeito, funciona !!
-
-    // if($nota1 > 10 || $nota1 < 0 ){
-    //     media.value = "erro, nota invalida";
-    // }
-
-    // if($nota2 > 10 || $nota2 < 0 ){
-    //     media.value = "erro, nota invalida";
-    // }
-    
-    // if(media > 10 || media < 0){
-    //         $conceito.value = "erro, notas invalidas";
-    //     }else{
-    // }if(media == 0 || media < 3){
-    //     $conceito.value = "E"
-    // }else {
-    //     if(media == 3 || media < 5){
-    //         $conceito.value = "D"
-    //     }else{
-    //         if(media == 5 || media < 8){
-    //             $conceito.value = "C"
-    //         }else{
-    //             if(media == 8 || media < 10){
-    //                 $conceito.value = "B"
-    //             }else{
-    //                 if(media == 10){
-    //                     $conceito.value = "A"
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
-    
-
-    console.log($nome.value);
+    $situacao.value = verificarSituacao ( media );
 
     $media.value = media;
+
 }
 
-calcularConceito = () =>{
-    const media = document.getElementById ('media').value;
-    const $conceito = document.getElementById('conceito');
-    
-    if(media == 0 || media < 3){
-            $conceito.value = "E";
-         }else {
-            if(media == 3 || media < 5){
-                 $conceito.value = "D";
-             }else{
-                 if(media == 5 || media < 8){
-                     $conceito.value = "C";
-                 }else{
-                     if(media == 8 || media < 10){
-                         $conceito.value = "B";
-                     }else{
-                         if(media == 10){
-                             $conceito.value = "A";
-                         }
-                     }
-                 }
-             }
-         }
-    
+const defineConceito =  () => {
+    const media = document.getElementById ('media').value
+    const $conceito = document.getElementById ('conceito');
+
+    if ( media < 3 ) {
+        $conceito.value = "E";
+    }else if ( media < 5 ) {
+        $conceito.value = "D";
+    }else if ( media < 8 ) {
+        $conceito.value = "C";
+    }else if ( media < 10 ) {
+        $conceito.value = "B";
+    }else {
+        $conceito.value = "A";
+    }
 }
 
-
-
-const calcular = () =>{
+const exibirResultados = () => {
     exibirMedia();
-    calcularConceito();
+    defineConceito();
 }
 
-// const calcular2 = function () { // segunda maneira  de encurtar a função
-//     calcularConceito();
-//     calcularMedia;
-// }
-
-// const calcular3 = () => { // iremos usar esta maneira de criar uma função
-//     calcularMedia();
-//     calcularConceito();
-// }
-
-// function soma (a, b) {
-//     return a + b;
-// }
-
-//const soma2 = (a, b) => a + b;// uma das aneiras de encurtar a função. 
-
-//const soma2 = a => a + b; // funciona quando so tem um argumento, ele não precisa de parenteses.
-
-$calcular.addEventListener('click', calcular);
+const exibir = ( el ) =>   el.classList.add ( 'exibir' );
+const ocultar = ( el ) =>   el.classList.remove ( 'exibir' );
 
 
-
-
-
+$calcular.addEventListener('click', exibirResultados);
+$exibir.addEventListener('click',( ) => exibir ( $conteiner ));
+$ocultar.addEventListener('click',( ) => ocultar ( $conteiner ));
 
 
 
--- readme.md
+++ readme.md
@@ -1,2 +1,15 @@
 # Aulas front-end SENAI Jandira
 aulas realizadas no curso tecnico de desenvolvimento de sistemas no SENAI de jandira no de 2019, na disciplina de programação WEB front-end monistrada pelo professor Fernando Leonid
+
+# projeto - calculado de medias e concelhos 
+## capacidade
+
+## desenvolvimento de javaScript
+-criar variaveis
+-criar funções
+-arrow function
+-funções anônimas
+-callback
+-manipular elementos de desenvolvimento
+-adicionar e remover classes CSS
+-ternário
